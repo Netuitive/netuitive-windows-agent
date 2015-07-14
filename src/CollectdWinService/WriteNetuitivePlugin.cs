641983d0cfadd65f4317e2b3bf258c8614c0fa2f
Andrew Paine <apaine@netuitive.com>
2015-07-14 12:49:47 +0100

    Modified to handle CollectableValue - can handle both MetricValue and AttributeValue

--- src/CollectdWinService/WriteNetuitivePlugin.cs
+++ src/CollectdWinService/WriteNetuitivePlugin.cs
@@ -3,30 +3,25 @@ using NLog;
 using System.Configuration;
 using System.Net;
 using BloombergFLP.CollectdWin;
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 
 namespace Netuitive.CollectdWin
 {
-    internal class WriteNetuitivePlugin : IMetricsWritePlugin
+    internal class WriteNetuitivePlugin : ICollectdWritePlugin
     {
 
-        private const string NetuitiveJsonFormatx =
-    @"{{""host"":""{0}"", ""plugin"":""{1}"", ""plugin_instance"":""{2}""," +
-    @" ""type"":""{3}"", ""type_instance"":""{4}"", ""time"":{5}, ""interval"":{6}," +
-    @" ""dstypes"":[{7}], ""dsnames"":[{8}], ""values"":[{9}]}}";
-
-//        private const string NetuitiveMetricFormat = @"{{""id"":""{0}"", ""type"":""{1}"", ""sparseDataStrategy"":""{2}"", ""unit"":""{3}"", ""name"":""{4}""}}";
         private const string NetuitiveMetricFormat = @"{{""id"":""{0}"", ""unit"":""{1}""}}";
 
         private const string NetuitiveSampleFormat = @"{{""metricId"":""{0}"", ""timestamp"":{1}, ""val"":{2}}}";
 
+
         private const string NetuitiveJsonFormat =
          @"[{{""type"": ""{0}"", ""id"":""{1}"", ""name"":""{2}"", ""location"":""{3}""" + 
          @", ""metrics"":[{4}]" +  
-         @", ""samples"":[{5}]" + 
+         @", ""samples"":[{5}]" +
+         @", ""attributes"":[{6}]" + 
         // @", ""attributes"":[{""name"":""testattr"",""value"":""testattrval""}]" + 
         // @", ""tags"": [{""name"":""testtag"", ""value"":""testtagval""}]" + 
          @"}}] ";
@@ -75,9 +70,9 @@ namespace Netuitive.CollectdWin
             Logger.Info("WriteNetuitive plugin stopped");
         }
 
-        public void Write(MetricValue metric)
+        public void Write(CollectableValue value)
         {
-            string payload = GeNetuitiveJsonStr(metric);
+            string payload = GeNetuitiveJsonStr(value);
             Logger.Debug("WriteNetuitive: {0}", payload);
             
             string result = "";
@@ -95,66 +90,67 @@ namespace Netuitive.CollectdWin
                 if (baseex as ThreadInterruptedException != null) {
                     throw baseex;
                 } else
-                    Logger.Error("Error posting data", ex);
+                    Logger.Error("Error posting data: {0}", payload, ex);
             }
             Logger.Debug("response: {0}", result);
         }
 
-        public string GeNetuitiveJsonStr(MetricValue metric)
+        public string GeNetuitiveJsonStr(CollectableValue value)
         {
             var metricList = new List<string>();
             var sampleList = new List<string>();
+            var attributeList = new List<string>();
 
-            string metricId = metric.PluginName;
-            if (metric.PluginInstanceName.Length > 0)
-                metricId += "." + metric.PluginInstanceName;
-            if (metric.TypeInstanceName.Length > 0)
-                metricId += "." + metric.TypeInstanceName;
-
-            if (metric.Values.Length == 1)
+            if (value is AttributeValue)
             {
-                // Simple case - just one metric in type
-//                metricList.Add(string.Format(NetuitiveMetricFormat, metricId, "type", "sds", "unit", "name"));
-                metricList.Add(string.Format(NetuitiveMetricFormat, metricId, metric.TypeName));
-                sampleList.Add(string.Format(NetuitiveSampleFormat, metricId, (long)metric.Epoch * 1000, metric.Values[0]));
+                AttributeValue attr = (AttributeValue)value;
+                attributeList.Add(attr.getJSON());
             }
-            else if (metric.Values.Length > 1)
+            else if (value is MetricValue)
             {
-                // Compound type with multiple metrics
-                IList<DataSource> dsList = DataSetCollection.Instance.GetDataSource(metric.TypeName);
-                if (dsList == null)
+                MetricValue metric = (MetricValue)value;
+                string metricId = metric.PluginName;
+                if (metric.PluginInstanceName.Length > 0)
+                    metricId += "." + metric.PluginInstanceName;
+                if (metric.TypeInstanceName.Length > 0)
+                    metricId += "." + metric.TypeInstanceName;
+
+                if (metric.Values.Length == 1)
                 {
-                    Logger.Debug("Invalid type : {0}, not found in types.db", metric.TypeName);
+                    // Simple case - just one metric in type
+                    metricList.Add(string.Format(NetuitiveMetricFormat, metricId, metric.TypeName));
+                    sampleList.Add(string.Format(NetuitiveSampleFormat, metricId, (long)metric.Epoch * 1000, metric.Values[0]));
                 }
-                else
+                else if (metric.Values.Length > 1)
                 {
-                    int ix = 0;
-                    foreach (DataSource ds in dsList)
+                    // Compound type with multiple metrics
+                    IList<DataSource> dsList = DataSetCollection.Instance.GetDataSource(metric.TypeName);
+                    if (dsList == null)
                     {
-                        // Include the Types.db suffix in the metric name
-                        metricList.Add(string.Format(NetuitiveMetricFormat, metricId, metric.TypeName));
-//                        metricList.Add(string.Format(NetuitiveMetricFormat, metricId + "." + ds.Name, "type", "sds", "unit", "name"));
-                        sampleList.Add(string.Format(NetuitiveSampleFormat, metricId + "." + ds.Name, (long)metric.Epoch * 1000, metric.Values[ix++]));
+                        Logger.Debug("Invalid type : {0}, not found in types.db", metric.TypeName);
+                    }
+                    else
+                    {
+                        int ix = 0;
+                        foreach (DataSource ds in dsList)
+                        {
+                            // Include the Types.db suffix in the metric name
+                            metricList.Add(string.Format(NetuitiveMetricFormat, metricId, metric.TypeName));
+                            sampleList.Add(string.Format(NetuitiveSampleFormat, metricId + "." + ds.Name, (long)metric.Epoch * 1000, metric.Values[ix++]));
+                        }
                     }
                 }
             }
-
             string metricsString = string.Join(",",  metricList.ToArray());
             string samplesString = string.Join(",", sampleList.ToArray());
+            string attributesString = string.Join(",", attributeList.ToArray());
 
-            string res = string.Format(NetuitiveJsonFormat, _elementType, metric.HostName, metric.HostName, _location,
+            string res = string.Format(NetuitiveJsonFormat, _elementType, value.HostName, value.HostName, _location,
                 metricsString,
-                samplesString);
+                samplesString, 
+                attributesString);
             return (res);
         
-/*                private const string NetuitiveJsonFormat =
-         @"[{""type"": ""{0}"", ""id"":""{1}"", ""name"":""{2}"", ""location"":""{3}""" + 
-         @", ""metrics"":[[""id"":""{4}"", ""type"":""{5}"", ""sparseDataStrategy"":""{6}"", ""unit"":""{7}"", ""name"":""{8}""]]" +  
-         @", ""samples"":[[""metricId"":""{9}"", ""timestamp"":""{10}"", ""val"":{11}]]" + 
-        // @", ""attributes"":[{""name"":""testattr"",""value"":""testattrval""}]" + 
-        // @", ""tags"": [{""name"":""testtag"", ""value"":""testtagval""}]" + 
-         @"}] ";
- */       
         }
     }
 
