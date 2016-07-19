d918f69543d04c1dc6312e5466d1c36facca5e9c
Andrew Paine <adnpaine@yahoo.co.uk>
2016-07-19 13:13:15 +0100

    Made ram suffix consistent with server element

--- src/CollectdWinService/ReadWindowsAttributesPlugin.cs
+++ src/CollectdWinService/ReadWindowsAttributesPlugin.cs
@@ -159,15 +159,15 @@ namespace Netuitive.CollectdWin
             return values;
         }
 
-        static String BytesToString(long byteCount)
+        private String BytesToString(long numBytes)
         {
-            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
-            if (byteCount == 0)
-                return "0" + suf[0];
-            long bytes = Math.Abs(byteCount);
+            string[] suffix = { " B", " KB", " MB", " GB", " TB", " PB"};
+            if (numBytes == 0)
+                return "0" + suffix[0];
+            long bytes = Math.Abs(numBytes);
             int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
             double num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+            return (Math.Sign(numBytes) * num).ToString() + suffix[place];
         }
 
         private IList<CollectableValue> GetCommonAttributes()
